1ec49d88b63d158e48ee5291f13fc0107df90882
Constantinos Paparas <paparas.constantinos@fullsix.com>
2017-11-15 20:01:21 +0100

    Add css styles

--- index.php
+++ index.php
@@ -3,12 +3,40 @@
 <head>
 	<meta charset="utf-8">
 	<title>Accueil admin danses</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=GFS+Didot|Hammersmith+One" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div id="sommaire">
-		<a href="groupes_liste.php">Liste Des Groupes</a>
-		<a href="profiles_liste.php">Liste des profiles</a>
-		<a href="cours_liste.php">Cours</a>
+
+	<header>
+		<h1 class="text-center">Bienvenu dans le gestionaire pour la Danse</h1>
+
+		
+</header>
+
+<main>
+
+	<nav id="sommaire" class="col-2">
+
+			<a href="groupes_liste.php">Liste Des Groupes</a>
+			<a href="profiles_liste.php">Liste des profiles</a>
+			<a href="cours_liste.php">Cours</a>
+	</nav>
+
+	<div class="col-10 item ">
+		<h2>Hello</h2>
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>	
 	</div>
+</main>
+<footer>
+		
+</footer>
 </body>
 </html>
