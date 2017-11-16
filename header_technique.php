3b71d723abec226959511dcbe46d78b9df2d8c48
Constantinos Paparas <paparas.constantinos@fullsix.com>
2017-11-16 11:37:01 +0100

    Add technical header and profile form

---
+++ header_technique.php
@@ -0,0 +1,17 @@
+<?php
+	if (! isset($title)) {
+		$title = "Gestionnaire de danse - Accueil";
+	}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title><?=$title?></title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=GFS+Didot|Hammersmith+One" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/tables.css">
+	<link rel="stylesheet" href="css/transverse.css">
+</head>
