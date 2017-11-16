b52cb3de9f238678f60101ac5dfa66d91623a3f4
Constantinos Paparas <paparas.constantinos@fullsix.com>
2017-11-16 11:37:01 +0100

    Add technical header and profile form

---
+++ form_profile.php
@@ -0,0 +1,104 @@
+<?php
+require('database.php');
+
+if (isset($_GET['id_profile'])) {
+	$query = $pdo->prepare('SELECT * FROM profiles WHERE id =?');
+
+	$query->execute([$_GET['id_profile']]);
+
+	$profile = $query->fetch(PDO::FETCH_ASSOC);
+
+}
+
+$queryGrpe = $pdo->prepare('SELECT * FROM groupes');
+$queryGrpe->execute();
+$groupes = $queryGrpe->fetch(PDO::FETCH_ASSOC);
+
+$title = "Gestionnaire de danse - Formulaire de profile";
+include('header_technique.php');
+?>
+
+<body>
+	<?php
+		$h1 = "Gestion du profile";
+		include('header.php');
+	?>
+	<main>
+		<?php 
+			include('nav.php');
+		?>
+		<div class="col-10 item ">
+			<form id="form_profile" method="POST" action="save_profile.php">
+				<fieldset>
+					<input type="hidden" name="id" id="id" value="<?=isset($_GET["id_profile"]) ? $_GET["id_profile"] : "" ?>">
+					<input type="hidden" name="date_creation" id="date_creation" value="<?=isset($profile["date_creation"]) ? $profileET["date_creation"] : "" ?>">
+					<div class="form-group">
+						<label for="email">Email * :</label>
+						<input class='form-control' type="email" name="email" id="email" size="50" value="<?= isset($profile['email']) ? $profile['email'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="nom">Nom * :</label>
+						<input class='form-control' type="text" name="nom" id="nom" size="50" value="<?= isset($profile['nom']) ? $profile['nom'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="prenom">Prénom * :</label>
+						<input class='form-control' type="text" name="prenom" id="prenom" size="50" value="<?= isset($profile['prenom']) ? $profile['prenom'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="date_naissance">Date de naissance :</label>
+						<input class='form-control' type="date" name="date_naissance" id="date_naissance" size="50" value="<?= isset($profile['date_naissance']) ? $profile['date_naissance'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="date_arrivee">Date d'arrivée :</label>
+						<input class='form-control' type="date" name="date_arrivee" id="date_arrivee" size="50" value="<?= isset($profile['date_arrivee']) ? $profile['date_arrivee'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="date_depart">Date de départ :</label>
+						<input class='form-control' type="date" name="date_depart" id="date_depart" size="50" value="<?= isset($profile['date_depart']) ? $profile['date_depart'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="adresse">Adresse :</label>
+						<input class='form-control' type="text" name="adresse" id="adresse" value="<?= isset($profile['adresse']) ? $profile['adresse'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="code_postal">Code Postal :</label>
+						<input class='form-control' type="text" name="code_postal" id="code_postal" size="5" value="<?= isset($profile['code_postal']) ? $profile['code_postal'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="ville">Ville :</label>
+						<input class='form-control' type="text" name="ville" id="ville" value="<?= isset($profile['ville']) ? $profile['ville'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="pays">Pays :</label>
+						<input class='form-control' type="text" name="pays" id="pays" value="<?= isset($profile['pays']) ? $profile['pays'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="telephone">Téléphone :</label>
+						<input class='form-control' type="text" placeholder="+33 6 24 55 88 88" name="telephone" id="telephone" value="<?= isset($profile['telephone']) ? $profile['telephone'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="pointure">Pointure :</label>
+						<input class='form-control' type="text" placeholder="43" name="pointure" id="pointure" value="<?= isset($profile['pointure']) ? $profile['pointure'] : '' ?>">
+					</div>
+					<div class="form-group">
+						<label for="taille">Taille (en cm) :</label>
+						<input class='form-control' type="text" placeholder="186" name="taille" id="taille" value="<?= isset($profile['taille']) ? $profile['taille'] : '' ?>">
+					</div>
+					<div class="checkbox">
+						<label for="professeur">Professeur</label>
+						<input type="checkbox" value="1" id="professeur" name="professeur" oncheck="alert('prof');">
+					</div>
+
+
+
+					 <div class="form-group">
+						<label for="commentaires">Commentaires :</label>
+						<textarea class="form-control" rows="5" id="commentaires" name="commentaires"></textarea>
+					</div> 
+					<input type="submit" name="submit_profile" value="Enregistrer">
+				</fieldset>
+			</form>
+		</div>
+	</main>
+</body>
+</html>
--- profiles_liste.php
+++ profiles_liste.php
@@ -4,19 +4,10 @@ $query = $pdo->prepare('SELECT * FROM `profiles`');
 
 $query->execute();
 $profiles = $query->fetchAll(PDO::FETCH_ASSOC);
+
+$title = "Gestionnaire de danse - Formulaire de profile";
+include('header_technique.php');
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<title>Liste des profiles</title>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-	<link href="https://fonts.googleapis.com/css?family=GFS+Didot|Hammersmith+One" rel="stylesheet"> 
-	<link rel="stylesheet" href="css/style.css">
-	<link href="css/transverse.css" rel="stylesheet">
-	<link href="css/tables.css" rel="stylesheet">
-</head>
 <body>
 	<?php
 		$h1 = "Liste des profiles";
