33c2a79139e431503e9b2a9e39e2fdc0e1a9e88e
Constantinos Paparas <paparas.constantinos@fullsix.com>
2017-12-08 10:53:34 +0100

    Orthographe

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 
 <nav id="sommaire" class="col-2">
     <a href="groupes_liste.php">Groupes</a>
-    <a href="profiles_liste.php">Profiles</a>
+    <a href="profiles_liste.php">Profils</a>
     <a href="cours_liste.php">Cours</a>
     <a href="liste_events.php">Evénements</a>
     <a href="contact_liste.php">Contacts</a>
